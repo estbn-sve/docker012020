eabfcd213b534acbd3d8deb4edbdb19f9aa08589
Pierre Sablé <psable@dawan.fr>
2020-01-14 10:21:41 +0100

    TP3 : second conteneur php

--- TP3_service.sh
+++ TP3_service.sh
@@ -3,6 +3,11 @@
 # Nettoyage
 docker stop app_web 2> /dev/null
 docker rm app_web 2> /dev/null
+docker stop app_php 2> /dev/null
+docker rm app_php 2> /dev/null
+
+# Conteneur php
+docker run --name app_php -d -v ${PWD}/www/:/srv/http/ phpdockerio/php73-fpm
 
 # Conteneur front
-docker run --name app_web --detach -p 80:80 -v ${PWD}/www/:/usr/share/nginx/html/ nginx:1.17
+docker run --name app_web --detach -p 80:80 --link app_php:php7 -v ${PWD}/www/:/usr/share/nginx/html/ nginx:1.17
---
+++ www/index.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1>Hello World from PHP7-FPM</h1>";
+echo 'Version PHP courante : ' . phpversion();
+?>
