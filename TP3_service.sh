3389b7d5ade90be8f24dc40d981c894ff7ae9247
Pierre Sablé <psable@dawan.fr>
2020-01-14 11:42:08 +0100

    TP3 : troisieme conteneur BDD

--- TP3_service.sh
+++ TP3_service.sh
@@ -5,9 +5,16 @@ docker stop app_web 2> /dev/null
 docker rm app_web 2> /dev/null
 docker stop app_php 2> /dev/null
 docker rm app_php 2> /dev/null
-
+docker stop app_bdd 2> /dev/null
+docker rm app_bdd 2> /dev/null
+# Conteneur de base de donnée 
+docker run --name app_bdd -d -e MYSQL_ROOT_PASSWORD=roottoor \
+                         -v ${PWD}/data:/var/lib/mysql:rw \
+                         mariadb:10.4
 # Conteneur php
-docker run --name app_php -d -v ${PWD}/www/:/srv/http/ phpdockerio/php73-fpm
+docker run --name app_php -d -v ${PWD}/www/:/srv/http/ \
+                      --link app_bdd:bdd \
+                      phpdockerio/php73-fpm
 
 # Conteneur front
 docker run --name app_web --detach -p 80:80 --link app_php:php7 \
