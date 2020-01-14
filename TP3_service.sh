08b89d1aefef773501f3551e3064ff2076145f48
Pierre Sablé <psable@dawan.fr>
2020-01-14 11:05:08 +0100

    TP3 : conf server_name

--- TP3_service.sh
+++ TP3_service.sh
@@ -10,4 +10,7 @@ docker rm app_php 2> /dev/null
 docker run --name app_php -d -v ${PWD}/www/:/srv/http/ phpdockerio/php73-fpm
 
 # Conteneur front
-docker run --name app_web --detach -p 80:80 --link app_php:php7 -v ${PWD}/www/:/usr/share/nginx/html/ nginx:1.17
+docker run --name app_web --detach -p 80:80 --link app_php:php7 \
+                    -v ${PWD}/conf/nginx.conf:/etc/nginx/conf.d/nginx.conf \
+                    -v ${PWD}/www/:/usr/share/nginx/html/ \
+                    nginx:1.17
