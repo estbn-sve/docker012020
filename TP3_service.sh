83fccc285c629147aacb97d708637a656a1a5b57
Pierre Sablé <psable@dawan.fr>
2020-01-14 14:04:59 +0100

    TP3 : Presentation des cgroup

--- TP3_service.sh
+++ TP3_service.sh
@@ -14,10 +14,12 @@ docker run --name app_bdd -d -e MYSQL_ROOT_PASSWORD=roottoor \
 # Conteneur php
 docker run --name app_php -d -v ${PWD}/www/:/srv/http/ \
                       --link app_bdd:bdd \
-                      phpdockerio/php73-fpm
+                      php-mysql:1.0
 
 # Conteneur front
 docker run --name app_web --detach -p 80:80 --link app_php:php7 \
                     -v ${PWD}/conf/nginx.conf:/etc/nginx/conf.d/nginx.conf \
                     -v ${PWD}/www/:/usr/share/nginx/html/ \
+                    --memory=512M \
+                    --cpus=0.5 \
                     nginx:1.17
