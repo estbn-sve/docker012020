0d51f8e9d007cc26e9fa123af8d57e8b354c58a7
Pierre Sablé <psable@dawan.fr>
2020-01-14 13:08:27 +0100

    TP3 : conf php pour connection bdd

--- www/index.php
+++ www/index.php
@@ -1,4 +1,19 @@
 <?php
 echo "<h1>Hello World from PHP7-FPM</h1>";
 echo 'Version PHP courante : ' . phpversion();
+
+echo '<pre>';
+try{
+  $conn = new \PDO('mysql:host=bdd:dbname=mysql', 'root', 'roottoor');
+  $sth = $conn->prepare('SHOW DATABASE');
+  $checks = $sth->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($checks as $check) {
+
+  print_r($check);
+  }
+}
+catch(\Exception $e){
+    print_r($e);
+}
+echo '</pre>';
 ?>
